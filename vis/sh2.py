33471a93aba45fc28b599e5660599096890ed216
Karl Zipser <karlzipser@berkeley.edu>
2024-01-01 03:26:14 -0800

    gacp

--- vis/sh2.py
+++ vis/sh2.py
@@ -1,13 +1,5 @@
 
-
-d=load_img_folder_to_dict(opjD('128sa/a'),maxnumfiles=35)
-
-
-
-def somehow_get_a_key():
-    return getch()
-
-
+from utilz2.vis.sh_ import *
 
 
 def menu(d):
@@ -31,24 +23,21 @@ def menu(d):
 
     fs=kys(d)
 
-    if len(fs)>len(kys2ctr):
-        cE('Warning, len(fs)>len(ks)')
-        len(fs)=len(kys2ctr)
+    numkys = len(fs)
+    if numkys>len(kys2ctr):
+        cE('Warning, numkys>len(ks)')
+        numkys=len(kys2ctr)
 
-    for k in kys(kys2ctr)[:len(fs)]:
+    for k in kys(kys2ctr)[:numkys]:
         selected[k]=False
 
-    lastkey=kys(selected)[0]
-
+    lastkey='0'
+    
     while True:
-
-        displaydic={}
-
-        for k in selected:
-
+        
+        clear_screen()
 
         assert lastkey in selected
-        clear_screen()    
 
         ctr=0
         for k in selected.keys():
@@ -61,21 +50,44 @@ def menu(d):
                 c='`--b'
             else:
                 c='`---'
-            clp(k,star,'\t',fs[ctr],c)#clp(k,star,'\t',fname(fs[ctr]),c)
+            clp(ctr+1,k,star,'\t',fname(fs[ctr]),c)
             ctr+=1
+        print("Press '`' to exit")
 
-        c=somehow_get_a_key()
+        displaydic={}
+        for k in sorted(kys(selected)):
+            
+            i=kys2ctr[k]
+            s=str(i)
+            displaydic[s]=1*d[fs[i]]
+            a=displaydic[s]
+            u=max(iwidth(a)//100,1)
+            if selected[k]:
+                s=s+'+'
+                a[:,-3*u:,:]=(255,0,0)
+                a[:,:3*u,:]=(255,0,0)
+                a[-3*u:,:,:]=(255,0,0)
+                a[:3*u,:,:]=(255,0,0)
+            if k==lastkey:
+                a[:,-u:,:]=(0,255,0)
+                a[:,:u,:]=(0,255,0)
+                a[-u:,:,:]=(0,255,0)
+                a[:u,:,:]=(0,255,0)
+        sh(displaydic,titles_font_scale=1,e=0)
+        spause()
+
+        c=getch()
 
         if c=='`':
             break
 
         elif c in selected.keys():
             lastkey=c
-            selected[c]=not selected[c]
+            #selected[c]=not selected[c]
 
         elif c==']':
             i=kys2ctr[lastkey]+1
-            if i in ctr2kys and i < len(fs):
+            if i in ctr2kys and i < numkys:
                 lastkey=ctr2kys[i]
 
         elif c=='[':
@@ -89,10 +101,17 @@ def menu(d):
     for k in selected:
         if selected[k]:
             selectedfs.append(fs[kys2ctr[k]])
-    return selectedfs
+    return selectedfs,list(set(fs)-set(selectedfs))
 
-s=menu()
-kprint(s)
+
+
+
+if __name__ == '__main__':
+    eg(__file__)
+    d=load_img_folder_to_dict(opjD('128sa/a'),maxnumfiles=8)
+    selected,notselected=menu(d)
+    kprint(selected,title='selected')
+    kprint(notselected,title='notselected')
 
 
 
