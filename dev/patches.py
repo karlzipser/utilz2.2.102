bb6410c810b4f4a25d7baa72652e21052d2befc7
Karl Zipser <karl.zipser.k@gmail.com>
2024-01-24 23:33:44 -0800

    gacp

--- dev/patches.py
+++ dev/patches.py
@@ -1,4 +1,4 @@
-
+"""
 if 'image' not in locals():
 	image = cv2.imread('large_image.jpg')
 
@@ -22,5 +22,62 @@ for i in range(num_patches_along_height):
 
 sh(patches_ijs)
 print(shape(image),len(patches_ijs))
+"""
 
-# reconstruct image from patches_ijs
+def getpatches(image,patch_size=(128,128),overlap_size=0):
+    num_patches_along_width = (image.shape[1] - patch_size[1]) // (patch_size[1] - overlap_size) + 1
+    num_patches_along_height = (image.shape[0] - patch_size[0]) // (patch_size[0] - overlap_size) + 1
+    patches = []
+    patches_ijs={}
+    for i in range(num_patches_along_height):
+        for j in range(num_patches_along_width):
+            start_i = i * (patch_size[0] - overlap_size)
+            start_j = j * (patch_size[1] - overlap_size)
+            patch = image[start_i:start_i + patch_size[0], start_j:start_j + patch_size[1]]
+            patches.append(patch)
+            patches_ijs[d2c(start_i,start_j)]=patch
+    return patches_ijs
+
+n=256
+imgs={}
+patches={}
+fs=sggo('/home/karl/Desktop/data/JPEM_2023-12-21-08-31-50/train/mask/*.png')
+for f in fs:
+    for k in ['mask','png']:
+        imgs[k]=rimread(f.replace('mask',k) )
+        imgs[k]=imgs[k][:(iheight(imgs[k])//n)*n,:(iwidth(imgs[k])//n)*n,:]
+    a=imgs['png']
+    a[:,:,2]*=0
+    b=imgs['mask'][:,:,0]
+    b[b<255]=0
+    a[:,:,0]=b
+    patches=getpatches(a,patch_size=(n,n),overlap_size=n//2)
+    CA()
+    sh(patches,r=0)
+
+    dst1=opjD('data/JPEM_2023-12-21-08-31-50/train-patches/1')
+    dst0=opjD('data/JPEM_2023-12-21-08-31-50/train-patches/0')
+    mkdirp(dst1)
+    mkdirp(dst0)
+    for p in patches:
+        p128=patches[p][(n//4):(-n//4),(n//4):(-n//4),0]
+        sh(p128,99)
+        a=intr(np.sqrt(sum(p128.flatten()/255)))
+        print(shape(p128),a)
+        img=1*patches[p]
+        img[:,:,0]=img[:,:,1]
+        img[:,:,2]=img[:,:,1]
+        if a<4:
+            dst=dst0
+        else:
+            dst=dst1
+        rimsave(opj(dst,fnamene(f)+'-'+p+'.png'),img)
+
+
+"""
+
+masks=sggo(opjD('data/JPEM_2023-12-21-08-31-50/train/mask/*.png'))
+for f in masks:
+    print(f)
+    assert ope(f.replace('mask','png'))
+"""
--- dev/squeezenetdev.py
+++ dev/squeezenetdev.py
@@ -10,6 +10,9 @@ from torch.autograd import Variable
 def f___weights_init(m):
     classname = m.__class__.__name__
     if classname.find('Conv') != -1:
+        #cm(classname,r=1)
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    if classname.find('ConvTranspose2d') != -1:
         nn.init.normal_(m.weight.data, 0.0, 0.02)
     elif classname.find('BatchNorm') != -1:
         nn.init.normal_(m.weight.data, 1.0, 0.02)
@@ -41,54 +44,77 @@ dscl=Describe_Layer
 class Fire(nn.Module):
 
     def __init__(
-        self,
+        _,
         inplanes,
         squeeze_planes,
         expand1x1_planes,
-        expand3x3_planes
+        expand3x3_planes,
+        godown=False,
+        goup=False,
     ):
-        super(Fire, self).__init__()
-        self.inplanes = inplanes
-        self.squeeze = nn.Conv2d(inplanes, squeeze_planes, kernel_size=1)
-        self.squeeze_activation = nn.ReLU(inplace=True)
-        self.expand1x1 = nn.Conv2d(squeeze_planes, expand1x1_planes, kernel_size=1)
-        self.expand1x1_activation = nn.ReLU(inplace=True)
-        self.expand3x3 = nn.Conv2d(squeeze_planes, expand3x3_planes, kernel_size=3, padding=1)
-        self.expand3x3_activation = nn.ReLU(inplace=True)
-    def forward(self, x):
-        x = self.squeeze_activation(self.squeeze(x))
+        super(Fire, _).__init__()
+        _.inplanes = inplanes
+        _.godown=godown
+        _.goup=goup
+        _.squeeze = nn.Conv2d(inplanes, squeeze_planes, kernel_size=1)
+        _.squeeze_activation = nn.ReLU(inplace=True)
+        _.expand1x1 = nn.Conv2d(squeeze_planes, expand1x1_planes, kernel_size=1)
+        _.expand1x1_activation = nn.ReLU(inplace=True)
+        _.expand3x3 = nn.Conv2d(squeeze_planes, expand3x3_planes, kernel_size=3, padding=1)
+        _.expand3x3_activation = nn.ReLU(inplace=True)
+        _.relu=nn.ReLU(inplace=True)
+        _.down=nn.Conv2d(squeeze_planes, squeeze_planes, kernel_size=3, stride=2, padding=1)
+        _.up=nn.ConvTranspose2d(squeeze_planes, squeeze_planes, kernel_size=3, stride=2, padding=0)
+
+    def forward(_, x):
+        x = _.squeeze_activation(_.squeeze(x))
+        if _.godown:
+            x=_.down(x)
+            x=_.relu(x)
+        if _.goup:
+            x=_.up(x)
+            x=_.relu(x)
+
         return torch.cat([
-            self.expand1x1_activation(self.expand1x1(x)),
-            self.expand3x3_activation(self.expand3x3(x))
+            _.expand1x1_activation(_.expand1x1(x)),
+            _.expand3x3_activation(_.expand3x3(x))
         ], 1)
 
 
-class SqueezeNet(nn.Module):
 
+class SqueezeNet_down(nn.Module):
     def __init__(
-        self,
+        _,
+        nin=3,
         n=8,
+        nout=1,
+        sigmoid=True,
     ):
-        super(SqueezeNet, self).__init__()
-        self.main1 = nn.Sequential(
-            nn.Identity(),                                          dscl('SqueezeNet input',True),
-            nn.Conv2d(1, 2*n, kernel_size=3, stride=2, padding=1),   dscl('\ta Conv2d output',True),
-            nn.ReLU(inplace=True),                                  dscl('\tb ReLU output',True),
-            nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),  dscl('\tc MaxPool2d output',True),
-            Fire(2*n, n, n, n),   
-            Fire(2*n, n, n, n),                                   dscl('\td Fire output',True),
-            nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),  dscl('\te MaxPool2d output',True),
-            Fire(2*n, n, n, n),                                   dscl('\td Fire output',True),
-            Fire(2*n, n, n, n),                                   dscl('\tf Fire output',True),
-            nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),  dscl('\tg MaxPool2d output',True),
-            Fire(2*n, n, n, n),
-            Fire(2*n, n, n, n),                                   dscl('\th Fire output',True),
-            nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),  dscl('\ti MaxPool2d output',True),
-            nn.Conv2d(2*n,1,kernel_size=1),                          dscl('\tj Conv2d output',True),
-            nn.AvgPool2d(kernel_size=3, stride=2),                  dscl('\tk AvgPool2d output',True),
-            nn.Sigmoid(),
+        super(SqueezeNet_down, _).__init__()
+        if sigmoid:
+            final=nn.Sigmoid
+        else:
+            final=nn.Identity
+        _.main1 = nn.Sequential(
+            nn.Identity(),                                              dscl('SqueezeNet input',True),
+            nn.Conv2d(nin, 2*n, kernel_size=3, stride=2, padding=1),    dscl('\ta Conv2d output',True),
+            nn.BatchNorm2d(2*n),
+            nn.ReLU(inplace=True),                                      dscl('\tb ReLU output',True),
+            
+            Fire(2*n, n, n, n,True),                                         dscl('\td Fire output',True),
+            Fire(2*n, 2*n, 2*n, 2*n),                               dscl('\te Fire output',True),
+            nn.BatchNorm2d(4*n),
+            Fire(4*n, 2*n, 2*n, 2*n,True),                               dscl('\tg Fire output',True),
+            Fire(4*n, 2*n, 4*n, 4*n),                               dscl('\th Fire output',True),
+            nn.BatchNorm2d(8*n),
+            Fire(8*n, 4*n, 4*n, 4*n,True),                               dscl('\tj Fire output',True),
+            Fire(8*n, 4*n, 8*n, 8*n,True),                               dscl('\tk Fire output',True),
+            nn.BatchNorm2d(16*n),
+            nn.Conv2d(16*n,nout,kernel_size=3,stride=2),                     dscl('\tm Conv2d output',True),
+            #nn.AvgPool2d(kernel_size=2, stride=2),                  dscl('\tn AvgPool2d output',True),
+            final(),
         )
-        for m in self.modules():
+        for m in _.modules():
             if isinstance(m, nn.Conv2d):
                 if False:#m is final_conv:
                     init.normal(m.weight.data, mean=0.0, std=0.01)
@@ -96,11 +122,147 @@ class SqueezeNet(nn.Module):
                     init.kaiming_uniform(m.weight.data)
                 if m.bias is not None:
                     m.bias.data.zero_()
+    def forward(_, x):
+        return _.main1(x)
 
 
-    def forward(self, x):
-        return self.main1(x)
 
+
+
+
+class SqueezeNet_deconv(nn.Module):
+    def __init__(
+        _,
+        nin=1,
+        n=8,
+        nout=1,
+    ):
+        super(SqueezeNet_deconv, _).__init__()
+        _.main1 = nn.Sequential(
+            nn.Identity(),                                          dscl('SqueezeNet_deconv input',True),
+            nn.ConvTranspose2d(nin, 16*n, kernel_size=3, stride=1, padding=0),   dscl('\ta ConvTranspose2d output',True),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(16*n, 8*n, kernel_size=3, stride=2, padding=0),   dscl('\tmp ConvTranspose2d output',True), 
+            nn.ReLU(inplace=True),                                 dscl('\tb ReLU output',True),
+            Fire(8*n,4*n, 4*n, 4*n,False,True),                                         dscl('\td up Fire output',True),
+            #Fire(2*n, 2*n, 2*n, 2*n),                                   dscl('\td Fire output',True),
+            nn.ConvTranspose2d(8*n, 4*n, kernel_size=3, stride=2, padding=0),   dscl('\tmp ConvTranspose2d output',True),
+            nn.ReLU(inplace=True),
+            #Fire(4*n, 2*n, 2*n, 2*n),                                   dscl('\td Fire output',True),
+            Fire(4*n, 2*n, 2*n, 2*n),                                   dscl('\tf Fire output',True),
+            nn.ConvTranspose2d(4*n, 2*n, kernel_size=3, stride=2, padding=0),   dscl('\tmp ConvTranspose2d output',True),
+            nn.ReLU(inplace=True),
+            #Fire(8*n, 4*n, 4*n, 4*n),
+            Fire(2*n, n, n, n),                                   dscl('\th Fire output',True),
+            nn.ConvTranspose2d(2*n, n, kernel_size=3, stride=2, padding=1),   dscl('\tmp ConvTranspose2d output',True),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(n,nout,kernel_size=4),                          dscl('\tj ConvTranspose2d output',True),
+
+        )
+        """
+        for m in _.modules():
+            if isinstance(m, nn.ConvTranspose2d):
+                if False:#m is final_conv:
+                    init.normal(m.weight.data, mean=0.0, std=0.01)
+                else:
+                    init.kaiming_uniform(m.weight.data)
+                if m.bias is not None:
+                    m.bias.data.zero_()
+        """
+    def forward(_, x):
+        return _.main1(x)
+
+
+
+
+
+
+
+class SqueezeNet_no_mp(nn.Module):
+    def __init__(
+        _,
+        nin=1,
+        n=8,
+        nout=1,
+        sigmoid=True,
+    ):
+        super(SqueezeNet_no_mp, _).__init__()
+        if sigmoid:
+            final=nn.Sigmoid
+        else:
+            final=nn.Identity
+        _.main1 = nn.Sequential(
+            nn.Identity(),                                              dscl('SqueezeNet input',True),
+            nn.Conv2d(nin, 2*n, kernel_size=3, stride=2, padding=1),    dscl('\ta Conv2d output',True),
+            nn.ReLU(inplace=True),                                      dscl('\tb ReLU output',True),
+            nn.Conv2d(2*n, 2*n, kernel_size=3, stride=2, padding=1),   dscl('\tmp Conv2d output',True),
+            Fire(2*n, n, n, n),                                         dscl('\td Fire output',True),
+            Fire(2*n, 2*n, 2*n, 2*n),                               dscl('\te Fire output',True),
+            nn.Conv2d(4*n, 4*n, kernel_size=3, stride=2, padding=1),   dscl('\tmp Conv2d output',True),
+            Fire(4*n, 2*n, 2*n, 2*n),                               dscl('\tg Fire output',True),
+            Fire(4*n, 2*n, 4*n, 4*n),                               dscl('\th Fire output',True),
+            nn.Conv2d(8*n, 8*n, kernel_size=3, stride=2, padding=1),   dscl('\tmp Conv2d output',True),
+            Fire(8*n, 4*n, 4*n, 4*n),                               dscl('\tj Fire output',True),
+            Fire(8*n, 4*n, 8*n, 8*n),                               dscl('\tk Fire output',True),
+            nn.Conv2d(16*n, 16*n, kernel_size=3, stride=2, padding=1),   dscl('\tmp Conv2d output',True),
+            nn.Conv2d(16*n,nout,kernel_size=3,stride=2),                     dscl('\tm Conv2d output',True),
+            final(),
+        )
+        for m in _.modules():
+            if isinstance(m, nn.Conv2d):
+                if False:#m is final_conv:
+                    init.normal(m.weight.data, mean=0.0, std=0.01)
+                else:
+                    init.kaiming_uniform(m.weight.data)
+                if m.bias is not None:
+                    m.bias.data.zero_()
+    def forward(_, x):
+        return _.main1(x)
+
+
+
+
+class SqueezeNet(nn.Module):
+    def __init__(
+        _,
+        nin=1,
+        n=8,
+        nout=1,
+        sigmoid=True,
+    ):
+        super(SqueezeNet, _).__init__()
+        if sigmoid:
+            final=nn.Sigmoid
+        else:
+            final=nn.Identity
+        _.main1 = nn.Sequential(
+            nn.Identity(),                                              dscl('SqueezeNet input',True),
+            nn.Conv2d(nin, 2*n, kernel_size=3, stride=2, padding=1),    dscl('\ta Conv2d output',True),
+            nn.ReLU(inplace=True),                                      dscl('\tb ReLU output',True),
+            nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),      dscl('\tc MaxPool2d output',True),
+            #Fire(2*n, n, n, n),                                         dscl('\td Fire output',True),
+            Fire(2*n, 2*n, 2*n, 2*n),                               dscl('\te Fire output',True),
+            nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),  dscl('\tf MaxPool2d output',True),
+            #Fire(4*n, 2*n, 2*n, 2*n),                               dscl('\tg Fire output',True),
+            Fire(4*n, 2*n, 4*n, 4*n),                               dscl('\th Fire output',True),
+            nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),  dscl('\ti MaxPool2d output',True),
+            #Fire(8*n, 4*n, 4*n, 4*n),                               dscl('\tj Fire output',True),
+            Fire(8*n, 4*n, 8*n, 8*n),                               dscl('\tk Fire output',True),
+            nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),  dscl('\tl MaxPool2d output',True),
+            nn.Conv2d(16*n,nout,kernel_size=2),                     dscl('\tm Conv2d output',True),
+            nn.AvgPool2d(kernel_size=2, stride=2),                  dscl('\tn AvgPool2d output',True),
+            final(),
+        )
+        for m in _.modules():
+            if isinstance(m, nn.Conv2d):
+                if False:#m is final_conv:
+                    init.normal(m.weight.data, mean=0.0, std=0.01)
+                else:
+                    init.kaiming_uniform(m.weight.data)
+                if m.bias is not None:
+                    m.bias.data.zero_()
+    def forward(_, x):
+        return _.main1(x)
 
 
 
@@ -111,33 +273,46 @@ def unit_test():
     
     CA()
     ns={}
-    for n in [3,6,8,9,10,11,12,13,14,15,16,32,64]:
-        test_net = SqueezeNet(n).to(device)
-        dts=[]
-        x=Variable(torch.randn(304, 1, 128, 128)).to(device)
-        #x=Variable(torch.randn(1, 1, 2464, 2064)).to(device)
-        for i in range(100):
-            t0=time.time()
-            a = test_net(x)
-            dt=time.time()-t0
-            #if dt>0.004 and dt < 0.02:
-            #    dts.append(1/dt)
-            dts.append(dt)
-            print(i,dt,5*' ',end='\r') 
-        #print('\nTested SqueezeNet.')
-        #hist(dts[1:],bins=1000)
-        m=np.median(dts)
-        ns[n]=m
-        #title(m)
-        #spause()
-        #cm(r=1)
+    nlist=[3,6,8,9,10,11,12,13,14,15,16,32,64]
+    for n in nlist:
+        try:
+            test_net = SqueezeNet(1,n,1).to(device)
+            dts=[]
+            x=Variable(torch.randn(304, 1, 128, 128)).to(device)
+            #x=Variable(torch.randn(1, 1, 2464, 2064)).to(device)
+            for i in range(100):
+                t0=time.time()
+                a = test_net(x)
+                dt=time.time()-t0
+                #if dt>0.004 and dt < 0.02:
+                #    dts.append(1/dt)
+                dts.append(dt)
+                print(i,dt,5*' ',end='\r') 
+            #print('\nTested SqueezeNet.')
+            #hist(dts[1:],bins=1000)
+            m=np.median(dts)
+            ns[n]=m
+            #title(m)
+            #spause()
+            #cm(r=1)
+        except:
+            break
     plot(list(ns.keys()),1/na(list(ns.values())),'o-')
     plt.xlabel('n')
     plt.ylabel('Hz')
+    plt.xlim(0,max(nlist))
 
+def unit_test2():
+    print(__file__,'unit_test()')
+    device = torch.device('cuda:0' if (torch.cuda.is_available()) else "cpu")
+    n=8
+    test_net = SqueezeNet_deconv(100,n,3).to(device)
+    x=Variable(torch.randn(304, 100, 1, 1)).to(device)
+    a = test_net(x)
+    print(a.size())
 
 if __name__=='__main__':
-    unit_test()
+    unit_test2()
     cm(r=1)
 
 
