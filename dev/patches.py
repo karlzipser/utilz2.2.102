3e4840c945a58a30c3588e6199a62a4b7921899e
Karl Zipser <karl.zipser.k@gmail.com>
2024-01-29 20:14:27 -0800

    gacp

--- dev/patches.py
+++ dev/patches.py
@@ -1,28 +1,4 @@
-"""
-if 'image' not in locals():
-	image = cv2.imread('large_image.jpg')
 
-image=image[:(2064//128)*128,:(2464//128)*128,:]
-
-patch_size = (128, 128)
-overlap_size = 0
-
-num_patches_along_width = (image.shape[1] - patch_size[1]) // (patch_size[1] - overlap_size) + 1
-num_patches_along_height = (image.shape[0] - patch_size[0]) // (patch_size[0] - overlap_size) + 1
-
-patches = []
-patches_ijs={}
-for i in range(num_patches_along_height):
-    for j in range(num_patches_along_width):
-        start_i = i * (patch_size[0] - overlap_size)
-        start_j = j * (patch_size[1] - overlap_size)
-        patch = image[start_i:start_i + patch_size[0], start_j:start_j + patch_size[1]]
-        patches.append(patch)
-        patches_ijs[d2c(start_i,start_j)]=patch
-
-sh(patches_ijs)
-print(shape(image),len(patches_ijs))
-"""
 
 def getpatches(image,patch_size=(128,128),overlap_size=0):
     num_patches_along_width = (image.shape[1] - patch_size[1]) // (patch_size[1] - overlap_size) + 1
@@ -73,10 +49,74 @@ for f in fs:
             dst=dst1
         rimsave(opj(dst,fnamene(f)+'-'+p+'.png'),img)
 
+#
+#########################
+#
+
+imgs={}
+fs=sggo(opjD('TEMP-outputs/*.pkl'))
+for q in fs:
+    o=lo(q)
+    for i in rlen(o['f']):
+        p=o['output'][i]
+        f=o['f'][i]
+        a=fnamene(f)
+        b=a.split('-')
+        imgname=b[0]
+        c=b[1].split(',')
+        x=int(c[0])
+        y=int(c[1])
+        if imgname not in imgs:
+            imgs[imgname]={}
+        imgs[imgname][(x,y)]=p
+
+
+        
+
+figure(1);
+mx=0
+for k in imgs:
+    png=rimread(opjD('data/JPEM_2023-12-21-08-31-50/train/png',k+'.png'))
+    clf()
+    sh(png)
+    plt_square()
+    for xy in imgs[k]:
+        x,y=xy
+        plot(y,x,'k.')
+    spause()
+    a=len(imgs[k].values())
+    mx=max(a,mx)
+    cm(k,a,r=1)
+print('max=',mx)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 """
 
-<<<<<<< HEAD
 # reconstruct image from patches_ijs
 
 
@@ -100,4 +140,4 @@ for f in masks:
     print(f)
     assert ope(f.replace('mask','png'))
 """
->>>>>>> ffd99dd98b0376a91bb8c47e370f1065be20a989
+
