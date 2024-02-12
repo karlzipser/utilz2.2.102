7628a95bc621c114a850991d513f5c8a44da8f65
Karl Zipser <karlzipser@berkeley.edu>
2024-02-12 10:51:53 -0800

    gacp

---
+++ scripts/rsyncs.sh
@@ -0,0 +1,15 @@
+#rsyncs
+
+rsync -ravL /Users/karlzipser/Pictures/Photos\ Library.photoslibrary  karl@192.168.2.212:/media/karl/ExtraDrive1/2024/2
+
+
+rse karl@192.168.2.102:knets Desktop
+
+
+rsync -ravL /Users/karlzipser/Library/Mobile\ Documents/com\~apple\~CloudDocs   karl@192.168.2.212:/media/karl/ExtraDrive1/2024/2
+rsync -ravL /Users/karlzipser/Movies karl@192.168.2.212:/media/karl/ExtraDrive1/2024/2
+rsync -ravL /Users/karlzipser/Desktop karl@192.168.2.212:/media/karl/ExtraDrive1/2024/2
+rsync -ravL /Users/karlzipser/BDD-presentations karl@192.168.2.212:/media/karl/ExtraDrive1/2024/2
+rsync -ravL /Users/karlzipser/music karl@192.168.2.212:/media/karl/ExtraDrive1/2024/2
+rsync -ravL /Users/karlzipser/data karl@192.168.2.212:/media/karl/ExtraDrive1/2024/2
+rsync -ravL /Users/karlzipser/various-nets karl@192.168.2.212:/media/karl/ExtraDrive1/2024/2
