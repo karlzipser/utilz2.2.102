f5720e4eaeb3cad0a62eedf65dce87865364f915
Karl Zipser <karlzipser@berkeley.edu>
2024-03-02 19:33:54 -0800

    gacp

--- dev/track2.py
+++ dev/track2.py
@@ -66,7 +66,7 @@ def generate_random_simple_boat(imgwidth):
 
 
 def xy2anchors(x,y,imgwidth,scale_factor):
-    xa,ya=intr(x/scale_factor),intr(y/scale_factor)
+    xa,ya=int(x/scale_factor),int(y/scale_factor)
     xb,yb=xa*scale_factor,ya*scale_factor
     dx,dy=xb-x,yb-y
     return xa,ya,dx,dy
@@ -77,12 +77,11 @@ def vectorize_boat(b,scale_factor):
     for kk in ['hull','wake']:
         h=b[kk]
         v=[]
-        ks=straskys('xanchor yanchor width height')
+        ks=straskys('width height')
         for k in ks:
             u=0
             if k in h:
                 u=h[k]
-            print('h',u)
             v.append(u)
         if 'xcenter' in h and 'ycenter' in h:
             x,y=h['xcenter'],h['ycenter']
@@ -93,43 +92,8 @@ def vectorize_boat(b,scale_factor):
         vb[kk]=dict(xy=(xa,ya),v=v)
     return vb
 
-def _vectorize_boat(b,scale_factor):
-    h=b['hull']
-    imgwidth=b['meta']['imgwidth']
-    #kprint(h,r=1)
-    w=b['wake']
-    vb={}
-    v=[]
-    ks=straskys('xanchor yanchor width height')
-    for k in ks:
-        u=0
-        if k in h:
-            u=h[k]
-        print('h',u)
-        v.append(u)
-    if 'xcenter' in h and 'ycenter' in h:
-        x,y=h['xcenter'],h['ycenter']
-    else:
-        x,y=0,0
-    xa,ya,dx,dy=xy2anchors(x,y,imgwidth,scale_factor)
-    v=[dx,dy]+v
-    vb['hull']=dict(xy=(xa,ya),v=v)
-    
-    v=[]
-    for k in ks:
-        u=0
-        if k in w:
-            u=w[k]
-        print('w',u)
-        v.append(u)
-    if 'xcenter' in h and 'ycenter' in w:
-        x,y=w['xcenter'],w['ycenter']
-    else:
-        x,y=0,0
-    xa,ya,dx,dy=xy2anchors(x,y,imgwidth,scale_factor)
-    v=[dx,dy]+v
-    vb['wake']=dict(xy=(xa,ya),v=v)
-    return vb
+
+
 def devectorize_boat(v):
     b=dict(
         hull=dict(
@@ -196,18 +160,20 @@ def plot_compound_object(obj,linetype):
 
 
 
-def get_noisy_version_of_compound_object(cobj,noisy_parameter):
+def get_noisy_version_of_compound_object(cobj,noise_parameter):
+    print('noise_parameter=',noise_parameter)
     o={}
     for k in cobj:
         if k!='meta':
-            o[k]=get_noisy_version_of_simple_object(cobj[k],noisy_parameter)
+            o[k]=get_noisy_version_of_simple_object(cobj[k],noise_parameter)
         else:
             o[k]=deepcopy(cobj[k])
     return o
 
 
-def get_noisy_version_of_simple_object(obj,noisy_parameter):
-    q=noisy_parameter
+def get_noisy_version_of_simple_object(obj,noise_parameter):
+    q=noise_parameter
+    print('q=',q)
     n=deepcopy(obj)
     if q and rnd()<obj['dropout_prob']:#[n['category']]:
         return {}
@@ -221,66 +187,232 @@ def get_noisy_version_of_simple_object(obj,noisy_parameter):
 
 
 
+
+
 if __name__ == '__main__':
 
     if True:
-        figure(1)
-        clf()
+
         imgwidth=256
         nboats=5#randint(1,5)
-        nsteps=20 #randint(5,30)
+        nsteps=9 #randint(5,30)
+        scale_factor=10
+        noise_parameter=5
         boats_through_time=[]
+
 
         for i in range(nboats):
             boat=generate_random_simple_boat(imgwidth)
             boats_through_time.append(
                 dict(
                     gt=[boat],
-                    noisy=[get_noisy_version_of_compound_object(boat,1.)],
+                    noisy=[get_noisy_version_of_compound_object(boat,noise_parameter)],
                 )
             )
 
-        for i in range(nsteps):
-            xylim(-1,imgwidth+1,-1,imgwidth+1)
-            plt_square()
 
+        for i in range(nsteps):
             for j in range(nboats):
                 boat=boats_through_time[j]
-                if i+1==nsteps:
-                    plot_compound_object(boat['gt'][0],'-')
-                plot_compound_object(boat['noisy'][0],':')
+                if nsteps>1:
+                    if len(boat['gt'])>=nsteps:
+                        boat['gt'].pop()
+                        boat['noisy'].pop()
+                        assert len(boat['gt'])==len(boat['noisy'])
+                    boat['gt'].insert(0,deepcopy(boat['gt'][0]))
+                    move_boat( boat['gt'][0] )
+                    noisy=get_noisy_version_of_compound_object(boat['gt'][0],noise_parameter)
+                    boat['noisy'].insert(0,noisy)
 
-                if len(boat['gt'])>=nsteps:
-                    boat['gt'].pop()
-                    boat['noisy'].pop()
-                    assert len(boat['gt'])==len(boat['noisy'])
-                boat['gt'].insert(0,deepcopy(boat['gt'][0]))
-                move_boat( boat['gt'][0] )
-                noisy=get_noisy_version_of_compound_object(boat['gt'][0],1.)
-                boat['noisy'].insert(0,noisy)
+        figure(101)
+        clf()
+        spause();
+        xylim(-1,imgwidth+1,-1,imgwidth+1)
+        plt_square()
+        for j in range(nboats):
+            boat=boats_through_time[j]
+            for i in range(nsteps):
+                plot_compound_object(boat['gt'][i],'-')
+        spause()
 
-            spause()
-            time.sleep(1/60)
 
-scale_factor=10
-A=zeros((imgwidth//scale_factor,imgwidth//scale_factor,nsteps*6))
 
-for b in boats_through_time:
-    k='gt'
-    for i in rlen(b[k]):
-        vb=vectorize_boat(b[k][i],scale_factor)
-        for l in vb:
-            x,y=vb[l]['xy']
-            v=vb[l]['v']
+def pixel2simple_boat(p):
+    pass
+
+def get_ps(img):
+    ps={}
+    #k='gt'
+    for x in range(shape(img)[0]):
+        for y in range(shape(img)[1]):
+            p=img[x,y,:]
+            if p.max()>0:
+                ps[(x,y)]=p
+
+    return ps
+
+
+
+final_imgs={}
+final_sums={}
+for k in ['gt']:#,'noisy']:
+    imgs=[]
+    for t in range(nsteps):
+        img=zeros((imgwidth//scale_factor,imgwidth//scale_factor,8))
+        for a_boat in boats_through_time:
+            noise_level=a_boat[k]
+            boat=noise_level[t]
+            kprint(boat)
+            b=vectorize_boat(boat,10)
+            x,y=b['hull']['xy']
             x=bound_value(x,0,imgwidth//scale_factor-1)
             y=bound_value(y,0,imgwidth//scale_factor-1)
-            x,y=int(x),int(y)
-            print(x,y,i)
-            A[x,y,i*len(v):(i+1)*len(v)]=v
+            img[x,y,:4]=b['hull']['v']
+            x,y=b['wake']['xy']
+            x=bound_value(x,0,imgwidth//scale_factor-1)
+            y=bound_value(y,0,imgwidth//scale_factor-1)
+            img[x,y,4:8]=b['wake']['v']
+        imgs.append(img)
+    final_img=np.concatenate(imgs,axis=2)
+    finalsum=np.abs(final_img).sum(axis=2)
+    finalsum[finalsum>0]=1
+    sh(np.rot90(finalsum),k)
+    final_imgs[k]=final_img
+    final_sums[k]=finalsum
 
-A[0,0,:]=0
-a=A.sum(axis=2)
-a[a>0]=1
-sh(np.rot90(a),2)
+#ps=get_ps(final_img)
+#pv=list(ps.values())
+
+"""
+figure(2);clf()
+for p in pv:
+    plot(p,'.-')
+    spause()
+    cm(r=1)
+"""
+
+
+objects={}
+indices = np.nonzero(finalsum)
+for i in rlen(indices[0]):
+    xa,ya=indices[0][i],indices[1][i]
+    for t in range(nsteps):
+        if t not in objects:
+            objects[t]=dict(
+                boats=[],
+                wakes=[],
+            )
+        a=t*8
+        b=t*8+4
+        c=t*8+4
+        d=t*8+8
+        p=final_img[xa,ya]
+        for category,q in zip(['boats','wakes'],[p[a:b],p[c:d]]):
+            #cm(t,category,q,r=0)
+            dx=q[0]
+            dy=q[1]
+            width=q[2]
+            height=q[3]
+            if width*height:
+                objects[t][category].append(
+                    dict(
+                        xcenter=xa*scale_factor-dx,
+                        ycenter=ya*scale_factor-dy,
+                        width=width,
+                        height=height,
+                    )
+                )
+kprint(objects)
+
+figure(101)
+for i in range(nsteps):
+    for j in range(nboats):
+        o=objects[i]['boats'][j]
+        plot_bounding_box(o,'k',':')
+        o=objects[i]['wakes'][j]
+        plot_bounding_box(o,'y',':')
+
+"""
+boats_through_time[
+    noise_levels t0{
+        gt[
+            simple_boat{
+                meta{}
+                hull{
+                    xcenter
+                    ycenter
+                    width
+                    height
+                }
+                wake{
+                    xcenter
+                    ycenter
+                    width
+                    height
+                }
+            }
+            simple_boat{
+                meta{}
+                hull{
+                    xcenter
+                    ycenter
+                    width
+                    height
+                }
+                wake{
+                    xcenter
+                    ycenter
+                    width
+                    height
+                }
+            }
+        ]
+        noisy[
+            ...
+        ]
+    }
+    noise_levels t1{
+        gt[
+            simple_boat{
+                meta{}
+                hull{
+                    xcenter
+                    ycenter
+                    width
+                    height
+                }
+                wake{
+                    xcenter
+                    ycenter
+                    width
+                    height
+                }
+            }
+            simple_boat{
+                meta{}
+                hull{
+                    xcenter
+                    ycenter
+                    width
+                    height
+                }
+                wake{
+                    xcenter
+                    ycenter
+                    width
+                    height
+                }
+            }
+        ]
+        noisy[
+            ...
+        ]
+    }
+}
+
+
+
+"""
+pass
 
 #EOF
