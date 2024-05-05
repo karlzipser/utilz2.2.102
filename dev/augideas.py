309442cf4febb4ea2b057b16a92feba522898d3d
Karl Zipser <karl.zipser.k@gmail.com>
2024-05-05 14:27:34 -0700

    gacp

--- dev/augideas.py
+++ dev/augideas.py
@@ -9,7 +9,7 @@ def augimg(img):
     if len(imgdic)>2:
         mask=an_element(imgdic)
         m=mask[:,:,0]
-        m[m>0.5]=1.
+        m[m>0.5]=255.
         m[m<1]=0.
         for i in range(3):
             mask[:,:,i]=m
@@ -17,7 +17,7 @@ def augimg(img):
         for i in range(2):
             a=bound_value(q*rndn(),-1,1)
             dimgs.append(a*an_element(imgdic))
-        new_img=0*img
+        new_img=1*img
         new_img+=mask*dimgs[0]
         new_img+=(1-mask)*dimgs[1]
         #sh(mask,2)
